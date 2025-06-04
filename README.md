ddad3c13824c383aea85b21474714cdecbc0bc2e
TylerLayton123 <112729158+TylerLayton123@users.noreply.github.com>
2025-06-04 13:20:54 -0400

    initial template for the website using react. Made with the help of AI

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is my personal website that I will be coding over the summer of 2025.
+
+npm install react react-dom react-router-dom
+npm install --save-dev react-scripts
+
+npm start
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="hero">
+        <h1>Welcome to My Personal Website</h1>
+        <p>I'm a passionate developer creating awesome things</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
